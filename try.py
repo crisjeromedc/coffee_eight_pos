57bbe6a750cf800e5a2d4aee5ae7c08321f54d4e
Jerome <crisjeromedc@gmail.com>
2024-06-03 15:16:04 +0800

    Comments added

--- try.py
+++
@@ -1,6 +0,0 @@
-from menu_base import MenuBase  # Importing the MenuBase class from the menu_base module
-
-class Menu(MenuBase):  # Defining the Menu class which inherits from the MenuBase class
-
-    def display(self):  # Defining the display method within the Menu class
-        self.display_menu()  # Calling the display_menu method from the MenuBase class
